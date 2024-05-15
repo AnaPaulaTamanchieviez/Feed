14a4a50946ea3dacfd172600d0e20cafb91efe35
Ana Paula Tamanchieviez <ana-paulatamanchieviez@hotmail.com>
2024-05-14 21:12:19 -0300

    Initial commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,47 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+export function Comment ({content, onDeleteComment}) {
+
+    const [likeCount, setLikeCount] = useState (0);
+
+
+    function handleDeleteComment () {
+        onDeleteComment (content)
+    }
+
+    function handleLikeComment () {
+        setLikeCount ((state) => {
+            return state + 1
+        });
+       // posso fazer somente assim tambem: setLikeCount(likeCount+1);
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/AnaPaulaTamanchieviez.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Ana Paula Tamanchieviez</strong>
+                            <time title='11 de maio às 18:47' dateTime='2024-05-11 18:57:45'>Cerca de 1h atrás</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title='Delete comment'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
